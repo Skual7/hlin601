901965d516d5c4631eea7c1f104d2ac3e8489122
llamalle1664 <lamallelouis@yahoo.fr>
2020-04-29 17:43:12 +0200

    ajoout de la sauvegarde d'un évenement

--- gestionv1/src/app/tournament-view/tournament-view.component.ts
+++ gestionv1/src/app/tournament-view/tournament-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../services/localstorage.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../services/request.service';
 
 @Component({
   selector: 'app-vue-generale',
@@ -10,11 +11,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 export class TournamentViewComponent implements OnInit {
 
-  constructor(private route : ActivatedRoute, private localStorageService : LocalStorageService,
+  constructor(private rs: RequestService, private route : ActivatedRoute, private localStorageService : LocalStorageService,
             private router : Router) { }
-  nameEv = "";
-  name ="";
-  rounds;
+  nameEv = "";//nom de l'évènement
+  name ="";//nom du tournoi
+  rounds;//rounds du tounoi
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['trn'];
@@ -39,10 +40,21 @@ export class TournamentViewComponent implements OnInit {
     }
   }
 
+  /* Méthode pour ajouter le round automatiquement si c'est possible. Met a jour les rounds*/
   addR(){
-    this.localStorageService.addRoundAutomate(this.name);
-    this.rounds = this.localStorageService.getRounds(this.name);
+    if(!this.localStorageService.addRoundAutomate(this.name)){
+       alert("Le round actuel n est pas fini ou le nombre de poule est inssuffisant pour passer au round suivant")
+      }
+    else {
+      this.rounds = this.localStorageService.getRounds(this.name);
+    }
   }
 
+  /* Méthode pour enregistrer la chaine du local storage dans la table évènement de la BDD*/
+  save(){
+    let str = JSON.stringify(window.localStorage.getItem("tournament"));
+    console.log(this.name+" "+str)
+    this.rs.request("INSERT INTO event (nom, string) VALUES ('"+this.name+"', '"+str+"')");
+  }
 
 }
